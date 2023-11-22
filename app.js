7206709d290ccdbdd9520cd53ec18ff20b83caee
kadir-zeren <k4dzeren@gmail.com>
2023-11-22 12:48:58 +0300

    v7

--- app.js
+++ app.js
@@ -41,19 +41,34 @@
 // colors[100] = "Pink";
 // console.log(colors);
 
-let a = 5;
-let b = a;
-console.log(a);
-console.log(b);
+// let a = 5;
+// let b = a;
+// console.log(a);
+// console.log(b);
 
-a = 6;
-console.log(b);
+// a = 6;
+// console.log(b);
 
-let arr1 = [1, 2, "Three", false];
-const arr2 = arr1;
+// let arr1 = [1, 2, "Three", false];
+// const arr2 = arr1;
 
-console.log(arr1);
-console.log(arr2);
-arr1[0] = 0;
-console.log(arr1);
-console.log(arr2);
+// console.log(arr1);
+// console.log(arr2);
+// arr1[0] = 0;
+// console.log(arr1);
+// console.log(arr2);
+
+const colors = ["Red", "Green", "Blue", "Yellow", "Brown"];
+const cls = colors.slice();
+console.log(colors);
+console.log(cls);
+
+colors[0] = "Black";
+console.log(colors);
+console.log(cls);
+console.log(colors.slice(2, 4));
+console.log(colors.slice(-2));
+console.log(colors.slice(2));
+console.log(colors.slice(8));
+console.log(colors.slice(-2, 1));
+console.log(colors.slice(-3, -1));
