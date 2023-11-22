80f8781f867396065f351b258248a7eac5c444e2
kadir-zeren <k4dzeren@gmail.com>
2023-11-22 11:23:59 +0300

    v2

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const arr = [100, "CW", false];
-console.log(arr);
-const dizi = new Array(100, "CW", true);
-console.log(dizi);
-const dizi2 = new Array(10);
-console.log(dizi2);
+// const arr = [100, "CW", false];
+// console.log(arr);
+// const dizi = new Array(100, "CW", true);
+// console.log(dizi);
+// const dizi2 = new Array(10);
+// console.log(dizi2);
+
+const colors = ["Red", "Green", "Blue"];
+console.log(colors[0]);
+console.log(colors[colors.length - 1]);
+console.log(colors[colors.length - 2]);
+console.log(colors[-1]);
+console.log(colors[56]);
